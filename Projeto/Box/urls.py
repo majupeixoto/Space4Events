0f707180734362fa4a5db710572d5bdaff4b3c13
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 10:05:17 -0300

    reorganizando pastas

--- Projeto/Box/urls.py
+++ Projeto/Box/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 from . import views
 
+# concentrar todos os views nesta pasta box
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('detalhes/', views.detalhes, name='detalhes'),
