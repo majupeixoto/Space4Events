f0602ed809460dd0a96136ca91c1ad9847d3eaae
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 11:59:48 -0300

    limpando importações no urls.py

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -1,6 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 from . import views
 
 # concentrar todos os views nesta pasta box, listar abaixo
