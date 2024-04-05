762225ca0b7500448907b421b49799786a60a2dd
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 20:42:43 -0300

    resolvendo um besteirol inacreditável na base.html

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('meus_espacos/', views.meus_espacos, name='meus_espacos'),
     path('visualizar_reservas/', views.visualizar_reservas, name='visualizar_reservas'),
     path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
-    path('cadastrar_espacos/', views.cadastrar_espaco, name='cadastrar_espacos'),
+    path('cadastrar_espaco/', views.cadastrar_espaco, name='cadastrar_espaco'),
 
     # path('nome_da_historia/', views.nome_da_historia, name="nome_da_historia"),
 ]
