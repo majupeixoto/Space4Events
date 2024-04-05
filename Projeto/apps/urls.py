7acc62e94c45e65dbed7cc525ad09c4a4828cd66
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 20:16:08 -0300

    resolvido o que foi perdido kkkk

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('detalhes/', views.detalhes, name='detalhes'),
+    path('favoritos/', views.favoritos, name='favoritos'),
+    path('meus_espacos/', views.meus_espacos, name='meus_espacos'),
     path('visualizar_reservas/', views.visualizar_reservas, name='visualizar_reservas'),
     path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
+    path('cadastrar_espacos/', views.cadastrar_espaco, name='cadastrar_espacos'),
+
     # path('nome_da_historia/', views.nome_da_historia, name="nome_da_historia"),
 ]
