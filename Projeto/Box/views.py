cd929a9af6c76135fd146398be3a5d5d987b47ec
Maria Júlia Peixoto <mjpo@cesar.school>
2024-04-05 10:47:26 -0300

    correção viwes

--- Projeto/Box/views.py
+++ Projeto/Box/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'box/home.html')
 
 def detalhes(request):
-    return render(request, 'detalhes.html')
+    return render(request, 'box/detalhes.html')
+
