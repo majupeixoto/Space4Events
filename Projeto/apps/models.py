06569deaf46f6652b76c38a6e2fddaaf6e7c543c
Thaís <thcba@cesar.school>
2024-04-05 18:58:23 -0300

    campo de foto para Rafa

--- Projeto/apps/models.py
+++ Projeto/apps/models.py
@@ -14,7 +14,7 @@ class Espaco(models.Model):
     numero_de_quartos = models.PositiveIntegerField(default=1)
     numero_de_banheiros = models.PositiveIntegerField(default=1)
     numero_de_hospedes = models.PositiveIntegerField(default=1)
-    # foto_principal = models.ImageField(upload_to='fotos/', null=True, blank=True)
+    foto_principal = models.ImageField(upload_to='fotos/', null=True, blank=True)
 
     def __str__(self):
         return self.nome
