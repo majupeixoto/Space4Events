ddd198cbe64dab282a51794e7e463a55c4d47531
Maria Júlia Peixoto <mjpo@cesar.school>
2024-04-05 11:23:17 -0300

    mudei o nome da box para apps

--- Projeto/apps/views.py
+++ Projeto/apps/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 def home(request):
-    return render(request, 'box/home.html')
+    return render(request, 'apps/home.html')
 
 def detalhes(request):
-    return render(request, 'box/detalhes.html')
+    return render(request, 'apps/detalhes.html')
 
