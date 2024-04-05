2bf6440a9a045c91ed2c732b8ae70519cb36ee8c
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 18:40:00 -0300

    correção de bugs e ajuste no template detalhes.html

--- Projeto/apps/views.py
+++ Projeto/apps/views.py
@@ -13,18 +13,14 @@ def listar_espacos(request):
     espacos = Espaco.objects.all()
     return render(request, 'apps/listar_espacos.html', {'espacos': espacos})
 
-def criar_reserva(request, espaco_id):
+def reservar_espaco(request, espaco_id):
+    espaco = get_object_or_404(Espaco, id=espaco_id)
+
     if request.method == 'POST':
         hospede_nome = request.POST.get('hospede_nome')
         data_check_in = request.POST.get('data_check_in')
         data_check_out = request.POST.get('data_check_out')
         numero_de_hospedes = int(request.POST.get('numero_de_hospedes'))
-
-        # Verificação - espaço existe?
-        try:
-            espaco = Espaco.objects.get(id=espaco_id)
-        except Espaco.DoesNotExist:
-            return HttpResponse("Espaço não encontrado")
 
         # Verificação - espaço comporta o número de pessoas?
         if espaco.numero_de_hospedes < numero_de_hospedes:
@@ -41,13 +37,13 @@ def criar_reserva(request, espaco_id):
             return HttpResponse("Espaço já reservado para as datas solicitadas!")
 
         # Criar a reserva
-        reserva = Reserva(espaco_proprietario_nome=espaco.proprietario_nome, espaco_nome=espaco.nome, hospede_nome=hospede_nome, data_check_in=data_check_in, data_check_out=data_check_out, numero_de_hospedes=numero_de_hospedes)
+        reserva = Reserva( espaco_proprietario_nome=espaco.proprietario_nome, espaco_nome=espaco.nome, hospede_nome=hospede_nome, data_check_in=data_check_in, data_check_out=data_check_out, numero_de_hospedes=numero_de_hospedes )
         reserva.save()
         
         return redirect('detalhes_espaco', espaco_id=espaco_id)
     else:
         espaco = Espaco.objects.get(id=espaco_id)
-        return render(request, 'reservar_espaco.html', {'espaco': espaco})
+        return render(request, 'apps/reservar_espaco.html', {'espaco': espaco})
 
 def visualizar_reservas(request):
     reservas = Reserva.objects.all()
