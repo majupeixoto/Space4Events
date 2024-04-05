246b1afdf88118c2c9f4b3933a18d6f2e069de58
Thaís <thcba@cesar.school>
2024-04-05 18:49:38 -0300

    tem um erro comentado

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('detalhes/', views.detalhes, name='detalhes'),
     path('visualizar_reservas/', views.visualizar_reservas, name='visualizar_reservas'),
     path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
-    path('reservar_espaco/<int:espaco_id>/', views.reservar_espaco, name='reservar_espaco'),
+    path('reservar_espaco/<int:espaco_id>/', views.reservar_espaco, name='reservar_espaco'), 
+    path('selecionar_espaco_para_reserva/', views.selecionar_espaco_para_reserva, name='selecionar_espaco_para_reserva'),
+
     # path('nome_da_historia/', views.nome_da_historia, name="nome_da_historia"),
 ]
