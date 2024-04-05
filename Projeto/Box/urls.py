d70a8c72551e4fb464e1dbfaee50b11d109fd0b2
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 10:31:32 -0300

    atualizando arquivo settings.py com o nome da pasta box em installed_apps

--- Projeto/Box/urls.py
+++ Projeto/Box/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from . import views
 
-# concentrar todos os views nesta pasta box
+# concentrar todos os views nesta pasta box, listar abaixo
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('detalhes/', views.detalhes, name='detalhes'),
