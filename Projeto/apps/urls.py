6a3517a74b8aea27eefec39df297ed21e1d4141f
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-05 16:32:41 -0300

    acrescentando CSS no <head> template base.html

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('detalhes/', views.detalhes, name='detalhes'),
     path('visualizar_reservas/', views.visualizar_reservas, name='visualizar_reservas'),
+    path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
     # path('nome_da_historia/', views.nome_da_historia, name="nome_da_historia"),
 ]
