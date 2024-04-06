df49768f533fe6c53a778588294595ae769d4ca9
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-06 11:53:57 -0300

    agora

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -4,12 +4,11 @@ from . import views
 # concentrar todos os views nesta pasta box, listar abaixo
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('detalhes/<int:id>/', views.detalhes, name='detalhes'),
+    path('cadastrar_espaco/', views.cadastrar_espaco, name='cadastrar_espaco'),
+    path('detalhes/', views.detalhes, name='detalhes'),
     path('favoritos/', views.favoritos, name='favoritos'),
+    path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
+    path('login/', views.login_view, name='login'),
     path('meus_espacos/', views.meus_espacos, name='meus_espacos'),
     path('visualizar_reservas/', views.visualizar_reservas, name='visualizar_reservas'),
-    path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
-    path('cadastrar_espaco/', views.cadastrar_espaco, name='cadastrar_espaco'),
-
-    # path('nome_da_historia/', views.nome_da_historia, name="nome_da_historia"),
 ]
