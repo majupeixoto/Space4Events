045870bf627378078bad86007a219dea8a64ada8
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-06 16:27:10 -0300

    alterações temporárias em views nas funções que pedem @login_required, tem que corrigir depois

--- Projeto/s4e/settings.py
+++ Projeto/s4e/settings.py
@@ -145,8 +145,7 @@ USE_TZ = True
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
