b7520371fcb50c9c52961af3074648d48aee34a7
TH <155742254+aguiarth@users.noreply.github.com>
2024-04-06 12:23:11 -0300

    função cadastro em views

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('detalhes/<int:id>/', views.detalhes, name='detalhes'),
     path('favoritos/', views.favoritos, name='favoritos'),
     path('listar_espacos/', views.listar_espacos, name='listar_espacos'),
-    path('login/', views.login_view, name='login'),
+    path('login/', views.login, name='login'),
     path('meus_espacos/', views.meus_espacos, name='meus_espacos'),
     path('visualizar_reservas/', views.visualizar_reservas, name='visualizar_reservas'),
 ]
