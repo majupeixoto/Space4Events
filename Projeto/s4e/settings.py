45ffa5b2bf67fde04993d620c55d0d87e7c0e819
Maria Júlia Peixoto <mjpo@cesar.school>
2024-04-27 12:58:24 -0300

    push antes da att

--- Projeto/s4e/settings.py
+++ Projeto/s4e/settings.py
@@ -152,7 +152,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
