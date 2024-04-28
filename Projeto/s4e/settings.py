a8f6ea103531a42120ec792b03044fada36995a4
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2024-04-28 20:30:59 -0300

    corrijindo bug das imagens

--- Projeto/s4e/settings.py
+++ Projeto/s4e/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+ 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
 STATICFILES_DIRS = [
