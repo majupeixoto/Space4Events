40f25330f330a10ee9848c8ab7122a524b9580ea
luana <llmaf@cesar.school>
2024-05-02 17:53:30 -0300

    minhas reservas

--- Projeto/cypress/e2e/minhas_reservas.cy.js
+++ Projeto/cypress/e2e/minhas_reservas.cy.js
@@ -12,10 +12,18 @@ describe('minhas_reservas', () => {
         cy.get(':nth-child(4) > input').type('lua');
         cy.get(':nth-child(5) > input').type('123');
         cy.get('.btn_login').click();
-        cy.get(':nth-child(1) > .card > .card-body > .d-flex > .btn-group > .btn').click();
+        cy.get(':nth-child(2) > .card > .card-body > .d-flex > .btn-group > .btn').click();
         cy.get(':nth-child(17) > .btn').click();
         cy.get('#hospede_nome').type('lua');
-        cy.get('#data_check_in').type('2024-05-02'); 
+        cy.get('#data_check_in').type('2024-04-30'); 
+        cy.get('#data_check_out').type('2024-05-01');
+        cy.get('#numero_de_hospedes').type('1');
+        cy.get('.btn_login').click();
+        cy.get('#numero_cartao').type('1243254');
+        cy.get('#data_validade').type('02/05');
+        cy.get('#cvv').type('123');
+        cy.get('.btn_login').click();
+     
     })
 
     it('Reserva em andamento', () => {
@@ -23,6 +31,9 @@ describe('minhas_reservas', () => {
     })
 
     it('Reserva ainda por vir', () => {
+        //steps do cenario3
+    })
+    it('Cancelamento de Reserva', () => {
         //steps do cenario3
     })
 })
