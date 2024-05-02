2e56657ca3d28250a9b950b4fa3a8f08c9a84d5a
luana <llmaf@cesar.school>
2024-05-02 12:46:11 -0300

    modificação no testes e Nome de usuário em cadastro.html

--- Projeto/cypress/e2e/minhas_reservas.cy.js
+++ Projeto/cypress/e2e/minhas_reservas.cy.js
@@ -1,7 +1,23 @@
 describe('minhas_reservas', () => {
     it('Reserva terminada', () => {
         cy.visit('/');
-        cy.get('.login').click()
+        cy.get('[href="/minhas_reservas/"]').click();
+        cy.get('.forget').click();
+        cy.get('#username').type('lua');
+        cy.get('#name').type('lua');
+        cy.get(':nth-child(5) > #email').type('lua@123.com');
+        cy.get('#password').type('123');
+        cy.get('.btn_login').click();
+        cy.get('p > a').click();
+        cy.get(':nth-child(4) > input').type('lua');
+        cy.get(':nth-child(5) > input').type('123');
+        cy.get('.btn_login').click();
+        cy.get(':nth-child(1) > .card > .card-body > .d-flex > .btn-group > .btn').click();
+        cy.get(':nth-child(17) > .btn').click();
+        cy.get('#hospede_nome').type('lua');
+        cy.get('#data_check_in').type('lua');
+        
+
     })
 
     it('Reserva em andamento', () => {
