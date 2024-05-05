74e6c8e685a2860a21f84271924cf99b860a6c19
Maria Júlia Peixoto <mjpo@cesar.school>
2024-05-05 14:34:57 -0300

    test fav e desf

--- Projeto/cypress/e2e/favoritos.cy.js
+++ Projeto/cypress/e2e/favoritos.cy.js
@@ -8,6 +8,7 @@ describe('test favoritar', () => {
         cy.get(':nth-child(2) > .card > .card-body > .d-flex > .btn-group > #visualizar_detalhes').click()
         cy.get('#favoritar').click()
         cy.get('[href="/favoritos/"]').click()
+        cy.get('.corpo > .row > :nth-child(2)').should('exist')
     })
     it('Desfavoritar com sucesso', ()=> {
         cy.visit('/');
@@ -15,11 +16,10 @@ describe('test favoritar', () => {
         cy.get(':nth-child(4) > input').type('b')
         cy.get(':nth-child(5) > input').type('123')
         cy.get('.btn_login').click()
-        cy.get(':nth-child(2) > .card > .card-body > .d-flex > .btn-group > #visualizar_detalhes').click()
-        cy.get('#favoritar').click()
         cy.get('[href="/favoritos/"]').click()
-        cy.get(':nth-child(3) > .card > .card-body > .btn').click()
+        cy.get(':nth-child(2) > .card > .card-body > .btn').click()
         cy.get('#desfavoritar').click()
         cy.get('[href="/favoritos/"]').click()
+        cy.get('.corpo > .row > :nth-child(2)').should('not.exist')
     })
 })
