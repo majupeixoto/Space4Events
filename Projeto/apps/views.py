3362232b6f387a8b965b096cc53397b97555bad4
gabrielken <gkci@cesar.school>
2024-05-04 23:09:10 -0300

    att views datetime

--- Projeto/apps/views.py
+++ Projeto/apps/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.urls import reverse
-from datetime import datetime
+import datetime
 
 
 def cadastro(request):
