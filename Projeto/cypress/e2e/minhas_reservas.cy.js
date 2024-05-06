cc9792829fe90faf734a00c6e0e691a170cc93db
luana <llmaf@cesar.school>
2024-05-06 13:27:24 -0300

    teste mudei para debito, credito e cancelamento de reserva

--- Projeto/cypress/e2e/minhas_reservas.cy.js
+++ Projeto/cypress/e2e/minhas_reservas.cy.js
@@ -1,4 +1,29 @@
-it('Reserva ainda por vir', () => {
+it('Debito', () => {
+    cy.visit('/');
+    cy.get('[href="/minhas_reservas/"]').click();
+    cy.get('.forget').click();
+    cy.get('#username').type('ricardo');
+    cy.get('#name').type('ricardo');
+    cy.get(':nth-child(5) > #email').type('ricardo@123.com');
+    cy.get('#password').type('123');
+    cy.get('.btn_login').click();
+    cy.get(':nth-child(2) > .card > .card-body > .d-flex > .btn-group > #visualizar_detalhes').click();
+    cy.get(':nth-child(7) > .btn').click();
+    cy.get('#hospede_nome').type('ricardo');
+    cy.get('#cpf').type('188.510.226-28');
+    cy.get('#data_check_in').type('2024-08-30'); 
+    cy.get('#data_check_out').type('2024-09-01');
+    cy.get('#numero_de_hospedes').type('1');
+    cy.get('.btn_login').click();
+    cy.get('#pills-debito-tab').click();
+    cy.get('#pills-debito > :nth-child(2) > #numero_cartao').type('1243254910112345');
+    cy.get(':nth-child(3) > #data_validade').type('02/25');
+    cy.get(':nth-child(4) > #cvv').type('123');
+    cy.get('#pills-debito > #botao_reservar').click();
+    cy.get('.active').click();
+
+})
+it('Credito', () => {
     cy.visit('/');
     cy.get('[href="/minhas_reservas/"]').click();
     cy.get('.forget').click();
@@ -7,36 +32,10 @@ it('Reserva ainda por vir', () => {
     cy.get(':nth-child(5) > #email').type('lua@123.com');
     cy.get('#password').type('123');
     cy.get('.btn_login').click();
-    cy.get('p > a').click();
-    cy.get(':nth-child(4) > input').type('lua');
-    cy.get(':nth-child(5) > input').type('123');
-    cy.get('.btn_login').click();
-    cy.get(':nth-child(2) > .card > .card-body > .d-flex > .btn-group > .btn').click();
-    cy.get(':nth-child(7) > .btn').click()
-    cy.get('#hospede_nome').type('lua');
-    cy.get('#cpf').type('188.509.226-28');
-    cy.get('#data_check_in').type('2024-08-30'); 
-    cy.get('#data_check_out').type('2024-09-01');
-    cy.get('#numero_de_hospedes').type('1');
-    cy.get('.btn_login').click();
-    cy.get('#pills-credito-tab').click();
-    cy.get('.payment-form > :nth-child(2) > #numero_cartao').type('1243254910112345');
-    cy.get(':nth-child(1) > #data_validade').type('02/25');
-    cy.get(':nth-child(2) > #cvv').type('123');
-    cy.get('.payment-form > #botao_reservar').click();
-    cy.get('.active').click();  
-})
-
-it('Cancelamento de Reserva', () => {
-    cy.visit('/');
-    cy.get('#entrar').click();
-    cy.get(':nth-child(4) > input').type('lua')
-    cy.get(':nth-child(5) > input').type('123');
-    cy.get('.btn_login').click();
-    cy.get(':nth-child(1) > .card > .card-body > .d-flex > .btn-group > #visualizar_detalhes').click();
+    cy.get(':nth-child(2) > .card > .card-body > .d-flex > .btn-group > #visualizar_detalhes').click();
     cy.get(':nth-child(7) > .btn').click();
     cy.get('#hospede_nome').type('lua');
-    cy.get('#cpf').type('188.509.226-28');
+    cy.get('#cpf').type('188.510.226-28');
     cy.get('#data_check_in').type('2024-08-30'); 
     cy.get('#data_check_out').type('2024-09-01');
     cy.get('#numero_de_hospedes').type('1');
@@ -47,7 +46,33 @@ it('Cancelamento de Reserva', () => {
     cy.get(':nth-child(2) > #cvv').type('123');
     cy.get('.payment-form > #botao_reservar').click();
     cy.get('.active').click();
-    cy.get(':nth-child(3) > .card > .card-body > .d-flex > .btn-group > form > #cancelar').click();
+})
+
+
+it('Cancelamento de Reserva', () => {
+    cy.visit('/');
+    cy.get('[href="/minhas_reservas/"]').click();
+    cy.get('.forget').click();
+    cy.get('#username').type('mari');
+    cy.get('#name').type('mari');
+    cy.get(':nth-child(5) > #email').type('mari@123.com');
+    cy.get('#password').type('123');
+    cy.get('.btn_login').click();
+    cy.get(':nth-child(1) > .card > .card-body > .d-flex > .btn-group > #visualizar_detalhes').click();
+    cy.get(':nth-child(7) > .btn').click();
+    cy.get('#hospede_nome').type('mari');
+    cy.get('#cpf').type('188.510.226-28');
+    cy.get('#data_check_in').type('2024-08-30'); 
+    cy.get('#data_check_out').type('2024-09-01');
+    cy.get('#numero_de_hospedes').type('1');
+    cy.get('.btn_login').click();
+    cy.get('#pills-credito-tab').click();
+    cy.get('.payment-form > :nth-child(2) > #numero_cartao').type('1243254910112345');
+    cy.get(':nth-child(1) > #data_validade').type('02/25');
+    cy.get(':nth-child(2) > #cvv').type('123');
+    cy.get('.payment-form > #botao_reservar').click();
+    cy.get('.active').click();
+    cy.get('#cancelar').click();
 })
 
 
