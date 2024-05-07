40453b4e93ecde2dd7671c774008f2a1890f7643
rbv2 <rbv2@cesar.school>
2024-05-07 01:11:10 -0300

    cypress

--- Projeto/cypress/e2e/pagamento.cy.js
+++ Projeto/cypress/e2e/pagamento.cy.js
@@ -7,6 +7,7 @@ describe('pagamento reserva', () => {
         cy.get('#name').type('thi');
         cy.get(':nth-child(5) > #email').type('thi@123.com');
         cy.get('#password').type('123');
+        cy.wait(3000);
         cy.get('.btn_login').click();
         cy.get('[aria-current="page"]').click();
         cy.get('#nome').type('Red House');
@@ -19,6 +20,7 @@ describe('pagamento reserva', () => {
         cy.get('#numero_de_quartos').type('3');
         cy.get('#numero_de_banheiros').type('2');
         cy.get('#numero_de_hospedes').type('9');
+        cy.wait(3000);
         cy.get('.container > form > .btn').click();
         cy.get('#logo > .fa-solid').click();
         cy.get('#sair').click();
@@ -28,6 +30,7 @@ describe('pagamento reserva', () => {
         cy.get('#name').type('ana');
         cy.get(':nth-child(5) > #email').type('ana@123.com');
         cy.get('#password').type('123');
+        cy.wait(3000);
         cy.get('.btn_login').click();
         cy.get('#visualizar_detalhes').click();
         cy.get(':nth-child(7) > .btn').click();
@@ -37,12 +40,15 @@ describe('pagamento reserva', () => {
         cy.get('#data_check_out').type('2024-09-24');
         cy.get('#numero_de_hospedes').type('4');
         cy.get('#additional_info').type('Um casal e 2 filhos');
+        cy.wait(3000);
         cy.get('.btn_login').click();
 
         cy.get('#pills-debito > :nth-child(2) > #numero_cartao').type('1111111111111111');
         cy.get(':nth-child(3) > #data_validade').type('0928');
         cy.get(':nth-child(4) > #cvv').type('111');
+        cy.wait(3000);
         cy.get('#pills-debito > #botao_reservar').click();
+    
         cy.get('.col').should('exist')
     })
 
