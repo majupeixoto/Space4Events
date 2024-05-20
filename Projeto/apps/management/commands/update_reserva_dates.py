1ebfbf867cafc775c8808eb7572f920d539bb84a
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2024-05-20 12:29:20 -0300

    att script para mudança das datas de check in e check out

--- Projeto/apps/management/commands/update_reserva_dates.py
+++ Projeto/apps/management/commands/update_reserva_dates.py
@@ -1,24 +1,27 @@
-# apps/management/commands/update_reserva_status.py
+# apps/management/commands/update_reserva_dates.py
 
 from django.core.management.base import BaseCommand
 from apps.models import Reserva  # Adjust the import according to your app structure
 
 class Command(BaseCommand):
-    help = 'Update the status of a reserva'
+    help = 'Update the check-in and check-out dates of a reserva'
 
     def add_arguments(self, parser):
         parser.add_argument('reserva_id', type=int, help='The ID of the reserva to update')
-        parser.add_argument('status', type=str, help='The new status of the reserva')
+        parser.add_argument('check_in', type=str, help='The new check-in date (YYYY-MM-DD)')
+        parser.add_argument('check_out', type=str, help='The new check-out date (YYYY-MM-DD)')
 
     def handle(self, *args, **kwargs):
         reserva_id = kwargs['reserva_id']
-        status = kwargs['status']
+        check_in = kwargs['check_in']
+        check_out = kwargs['check_out']
 
         try:
             reserva = Reserva.objects.get(id=reserva_id)
-            reserva.status = status
+            reserva.data_check_in = check_in
+            reserva.data_check_out = check_out
             reserva.save()
-            self.stdout.write(self.style.SUCCESS(f'Successfully updated reserva {reserva_id} to status "{status}"'))
+            self.stdout.write(self.style.SUCCESS(f'Successfully updated reserva {reserva_id} check-in to {check_in} and check-out to {check_out}'))
         except Reserva.DoesNotExist:
             self.stdout.write(self.style.ERROR(f'Reserva with id {reserva_id} does not exist'))
         except Exception as e:
