acd1f1965d465b3fccfcabf61a2774105e1e035c
luana <llmaf@cesar.school>
2024-06-03 15:01:37 -0300

    editar perfil agora está com cadastro proprório

--- Projeto/cypress/e2e/cadastro.cy.js
+++ Projeto/cypress/e2e/cadastro.cy.js
@@ -13,8 +13,11 @@ describe('test cadastro', () => {
     it('Editar Perfil', () => {
         cy.visit('/');
         cy.get('.ms-auto > .nav-link').click()
-        cy.get(':nth-child(4) > input').type('malu')
-        cy.get(':nth-child(5) > input').type('123')
+        cy.get('.forget').click()
+        cy.get('.login > :nth-child(3)').type('Marcos')
+        cy.get('.login > :nth-child(4)').type('Marcos Lima')
+        cy.get('.login > :nth-child(5)').type('marcos@gmail.com')
+        cy.get('.login > :nth-child(6)').type('123')
         cy.get('.btn_login').click()
         cy.get('.ms-auto > a.nav-link').click();
         cy.get('.btn-primary').click();
@@ -23,7 +26,7 @@ describe('test cadastro', () => {
         cy.get('#last_name').clear();
         cy.get('#last_name').type('Nome');
         cy.get('#email').clear();
-        cy.get('#email').type('malu2@gmail.com');
+        cy.get('#email').type('marcos2@gmail.com');
         cy.get('#senha_atual').type('123');
         cy.get('#nova_senha').type('1234');
         cy.get('#nova_senha_confirmacao').type('1234');
